zyphlar/organicmaps-test

[strings][routing] Add TTS announcement of street names as an option
Signed-off-by: Will Bradley <bradley.will@gmail.com>

routing/routing_tests/turns_tts_text_tests.cpp
@@ -156,6 +156,101 @@ UNIT_TEST(GetTtsTextTest)
   TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Затем. Поворот налево.", ());
 }
 
+UNIT_TEST(GetTtsStreetTextTest)
+{
+  string const engShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"In 300 meters.\",\
+      \"in_500_meters\":\"In 500 meters.\",\
+      \"then\":\"Then.\",\
+      \"onto\":\"onto\",\
+      \"make_a_right_turn\":\"Make a right turn.\",\
+      \"make_a_left_turn\":\"Make a left turn.\",\
+      \"dist_direction_onto_street\":\"%1$s %2$s %3$s %4$s\",\
+      \"you_have_reached_the_destination\":\"You have reached the destination.\"\
+      }";
+
+  string const jaShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"三百メートル先\",\
+      \"in_500_meters\":\"五百メートル先\",\
+      \"then\":\"その先\",\
+      \"onto\":\"に入ります\",\
+      \"make_a_right_turn\":\"右折です。\",\
+      \"make_a_left_turn\":\"左折です。\",\
+      \"make_a_right_turn_street\":\"右折し\",\
+      \"make_a_left_turn_street\":\"左折し\",\
+      \"dist_direction_onto_street\":\"%1$s%2$s %4$s %3$s\",\
+      \"you_have_reached_the_destination\":\"到着。\"\
+      }";
+
+  string const faShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"ﺩﺭ ﺲﯿﺻﺩ ﻢﺗﺮﯾ\",\
+      \"in_500_meters\":\"ﺩﺭ ﭖﺎﻨﺻﺩ ﻢﺗﺮﯾ\",\
+      \"then\":\"ﺲﭙﺳ\",\
+      \"onto\":\"ﺐﻫ\",\
+      \"make_a_right_turn\":\"ﺐﻫ ﺭﺎﺴﺗ ﺐﭙﯿﭽﯾﺩ.\",\
+      \"make_a_left_turn\":\"ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ.\",\
+      \"dist_direction_onto_street\":\"%1$s %2$s %3$s %4$s\",\
+      \"you_have_reached_the_destination\":\"ﺶﻣﺍ ﺮﺴﯾﺪﻫ ﺎﯾﺩ.\"\
+      }";
+
+  string const arShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"ﺐﻋﺩ ﺙﻼﺜﻤﺋﺓ ﻢﺗﺭ\",\
+      \"in_500_meters\":\"ﺐﻋﺩ ﺦﻤﺴﻤﺋﺓ ﻢﺗﺭ\",\
+      \"then\":\"ﺚﻣ\",\
+      \"onto\":\"ﺈﻟﻯ\",\
+      \"make_a_right_turn\":\"ﺎﻨﻌﻄﻓ ﻲﻤﻴﻧﺍ.\",\
+      \"make_a_left_turn\":\"ﺎﻨﻌﻄﻓ ﻲﺳﺍﺭﺍ.\",\
+      \"dist_direction_onto_street\":\"%1$s %2$s %3$s %4$s\",\
+      \"you_have_reached_the_destination\":\"ﻞﻗﺩ ﻮﺼﻠﺗ.\"\
+      }";
+
+  GetTtsText getTtsText;
+  // Notification(uint32_t distanceUnits, uint8_t exitNum, bool useThenInsteadOfDistance,
+  //    CarDirection turnDir, Settings::Units lengthUnits, std::string nextStreet)
+  Notification const notification1(500, 0, false, CarDirection::TurnRight,
+                                   measurement_utils::Units::Metric, "Main Street");
+  Notification const notification2(300, 0, false, CarDirection::TurnLeft,
+                                   measurement_utils::Units::Metric, "Main Street");
+  Notification const notification3(300, 0, false, CarDirection::TurnLeft,
+                                   measurement_utils::Units::Metric);
+  Notification const notification4(0, 0, true, CarDirection::TurnLeft,
+                                   measurement_utils::Units::Metric);
+
+  getTtsText.ForTestingSetLocaleWithJson(engShortJson, "en");
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "In 500 meters Make a right turn onto Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "In 300 meters Make a left turn onto Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "In 300 meters. Make a left turn.", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Then. Make a left turn.", ());
+
+  getTtsText.ForTestingSetLocaleWithJson(jaShortJson, "ja");
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "五百メートル先右折し Main Street に入ります", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "三百メートル先左折し Main Street に入ります", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "三百メートル先 左折です。", ()); // note the extraneous space here due to + " " +
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "その先 左折です。", ()); // note the extraneous space here due to + " " +
+
+  getTtsText.ForTestingSetLocaleWithJson(faShortJson, "fa");
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "ﺩﺭ ﭖﺎﻨﺻﺩ ﻢﺗﺮﯾ ﺐﻫ ﺭﺎﺴﺗ ﺐﭙﯿﭽﯾﺩ ﺐﻫ Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "ﺩﺭ ﺲﯿﺻﺩ ﻢﺗﺮﯾ ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ ﺐﻫ Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "ﺩﺭ ﺲﯿﺻﺩ ﻢﺗﺮﯾ ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ.", ()); // note the extraneous space here due to + " " +
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "ﺲﭙﺳ ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ.", ()); // note the extraneous space here due to + " " +
+
+  getTtsText.ForTestingSetLocaleWithJson(arShortJson, "ar");
+  //TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "بعد ستمئة قدم انعطف يمينا ﺈﻟﻯ Main Street", ());
+  //TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "بع ﺙﻼﺜﻤمئة قدم انعطف يمينا ﺈﻟﻯ Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "ﺐﻋﺩ ﺙﻼﺜﻤﺋﺓ ﻢﺗﺭ ﺎﻨﻌﻄﻓ ﻲﺳﺍﺭﺍ.", ()); // note the extraneous space here due to + " " +
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "ﺚﻣ ﺎﻨﻌﻄﻓ ﻲﺳﺍﺭﺍ.", ()); // note the extraneous space here due to + " " +
+
+  // TODO: make tests for Dutch (nl) with verb prefixes, and for new "take exit 123" syntax
+}
+
 UNIT_TEST(GetAllSoundedDistMetersTest)
 {
   VecPairDist const & allSoundedDistMeters = GetAllSoundedDistMeters();

zell-mbc/organicmaps

routing/routing_tests/turns_tts_text_tests.cpp
@@ -147,13 +147,108 @@ UNIT_TEST(GetTtsTextTest)
   TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "In 500 meters. Make a right turn.", ());
   TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "In 300 meters. Make a left turn.", ());
   TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "You have reached the destination.", ());
-  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Then. Make a left turn.", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Then.  Make a left turn.", ());
 
   getTtsText.ForTestingSetLocaleWithJson(rusShortJson, "ru");
   TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "Через 500 метров. Поворот направо.", ());
   TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "Через 300 метров. Поворот налево.", ());
   TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "Вы достигли конца маршрута.", ());
-  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Затем. Поворот налево.", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Затем.  Поворот налево.", ());
+}
+
+UNIT_TEST(GetTtsStreetTextTest)
+{
+  string const engShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"In 300 meters.\",\
+      \"in_500_meters\":\"In 500 meters.\",\
+      \"then\":\"Then.\",\
+      \"onto\":\"onto\",\
+      \"make_a_right_turn\":\"Make a right turn.\",\
+      \"make_a_left_turn\":\"Make a left turn.\",\
+      \"dist_direction_onto_street\":\"%1$s %2$s %3$s %4$s\",\
+      \"you_have_reached_the_destination\":\"You have reached the destination.\"\
+      }";
+
+  string const jaShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"三百メートル先\",\
+      \"in_500_meters\":\"五百メートル先\",\
+      \"then\":\"その先\",\
+      \"onto\":\"に入ります\",\
+      \"make_a_right_turn\":\"右折です。\",\
+      \"make_a_left_turn\":\"左折です。\",\
+      \"make_a_right_turn_street\":\"右折し\",\
+      \"make_a_left_turn_street\":\"左折し\",\
+      \"dist_direction_onto_street\":\"%1$s%2$s %4$s %3$s\",\
+      \"you_have_reached_the_destination\":\"到着。\"\
+      }";
+
+  string const faShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"ﺩﺭ ﺲﯿﺻﺩ ﻢﺗﺮﯾ\",\
+      \"in_500_meters\":\"ﺩﺭ ﭖﺎﻨﺻﺩ ﻢﺗﺮﯾ\",\
+      \"then\":\"ﺲﭙﺳ\",\
+      \"onto\":\"ﺐﻫ\",\
+      \"make_a_right_turn\":\"ﺐﻫ ﺭﺎﺴﺗ ﺐﭙﯿﭽﯾﺩ.\",\
+      \"make_a_left_turn\":\"ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ.\",\
+      \"dist_direction_onto_street\":\"%1$s %2$s %3$s %4$s\",\
+      \"you_have_reached_the_destination\":\"ﺶﻣﺍ ﺮﺴﯾﺪﻫ ﺎﯾﺩ.\"\
+      }";
+
+  string const arShortJson =
+      "\
+      {\
+      \"in_300_meters\":\"ﺐﻋﺩ ﺙﻼﺜﻤﺋﺓ ﻢﺗﺭ\",\
+      \"in_500_meters\":\"ﺐﻋﺩ ﺦﻤﺴﻤﺋﺓ ﻢﺗﺭ\",\
+      \"then\":\"ﺚﻣ\",\
+      \"onto\":\"ﺈﻟﻯ\",\
+      \"make_a_right_turn\":\"ﺎﻨﻌﻄﻓ ﻲﻤﻴﻧﺍ.\",\
+      \"make_a_left_turn\":\"ﺎﻨﻌﻄﻓ ﻲﺳﺍﺭﺍ.\",\
+      \"dist_direction_onto_street\":\"%1$s %2$s %3$s %4$s\",\
+      \"you_have_reached_the_destination\":\"ﻞﻗﺩ ﻮﺼﻠﺗ.\"\
+      }";
+
+  GetTtsText getTtsText;
+  // Notification(uint32_t distanceUnits, uint8_t exitNum, bool useThenInsteadOfDistance,
+  //    CarDirection turnDir, Settings::Units lengthUnits, std::string nextStreet)
+  Notification const notification1(500, 0, false, CarDirection::TurnRight,
+                                   measurement_utils::Units::Metric, "Main Street");
+  Notification const notification2(300, 0, false, CarDirection::TurnLeft,
+                                   measurement_utils::Units::Metric, "Main Street");
+  Notification const notification3(300, 0, false, CarDirection::TurnLeft,
+                                   measurement_utils::Units::Metric);
+  Notification const notification4(0, 0, true, CarDirection::TurnLeft,
+                                   measurement_utils::Units::Metric);
+
+  getTtsText.ForTestingSetLocaleWithJson(engShortJson, "en");
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "In 500 meters Make a right turn onto Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "In 300 meters Make a left turn onto Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "In 300 meters. Make a left turn.", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "Then.  Make a left turn.", ());
+
+  getTtsText.ForTestingSetLocaleWithJson(jaShortJson, "ja");
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "五百メートル先右折し Main Street に入ります", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "三百メートル先左折し Main Street に入ります", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "三百メートル先 左折です。", ()); // note the extraneous space here due to + " " +
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "その先  左折です。", ()); // note the extraneous spaces here due to + " " +
+
+  getTtsText.ForTestingSetLocaleWithJson(faShortJson, "fa");
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "ﺩﺭ ﭖﺎﻨﺻﺩ ﻢﺗﺮﯾ ﺐﻫ ﺭﺎﺴﺗ ﺐﭙﯿﭽﯾﺩ ﺐﻫ Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification2), "ﺩﺭ ﺲﯿﺻﺩ ﻢﺗﺮﯾ ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ ﺐﻫ Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "ﺩﺭ ﺲﯿﺻﺩ ﻢﺗﺮﯾ ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ.", ()); // note the extraneous space here due to + " " +
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "ﺲﭙﺳ  ﺐﻫ ﭻﭘ ﺐﭙﯿﭽﯾﺩ.", ()); // note the extraneous spaces here due to + " " +
+
+  getTtsText.ForTestingSetLocaleWithJson(arShortJson, "ar");
+  //TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "بعد ستمئة قدم انعطف يمينا ﺈﻟﻯ Main Street", ());
+  //TEST_EQUAL(getTtsText.GetTurnNotification(notification1), "بع ﺙﻼﺜﻤمئة قدم انعطف يمينا ﺈﻟﻯ Main Street", ());
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification3), "ﺐﻋﺩ ﺙﻼﺜﻤﺋﺓ ﻢﺗﺭ ﺎﻨﻌﻄﻓ ﻲﺳﺍﺭﺍ.", ()); // note the extraneous space here due to + " " +
+  TEST_EQUAL(getTtsText.GetTurnNotification(notification4), "ﺚﻣ  ﺎﻨﻌﻄﻓ ﻲﺳﺍﺭﺍ.", ()); // note the extraneous spaces here due to + " " +
+
+  // TODO: make tests for Dutch (nl) with verb prefixes, and for new "take exit 123" syntax
 }
 
 UNIT_TEST(GetAllSoundedDistMetersTest)